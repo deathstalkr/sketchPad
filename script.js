d402d784190a271aa4a573d77ec1eac56537494f
deathstalkr <solomon.johnson098@gmail.com>
2023-12-05 13:13:12 +0530

    added rainbow, eraser buttons and their logic

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-let grid = document.querySelector(".grid");
+const grid = document.querySelector(".grid");
 const gridSizeBtn = document.querySelector("#grid-size-btn");
-let gridSize = 16;
-let currentColor = "#333333";
-let colorPicker = document.getElementById("colorPicker");
+const colorPicker = document.getElementById("colorPicker");
+const rainbowBtn = document.getElementById("rainbow");
+const eraserBtn = document.getElementById("eraser");
 
-colorPicker.oninput = (e) => setCurrentColor(e.target.value);
+let currentMode = "default";
 
-addGrid(gridSize);
+colorPicker.oninput = (e) => {
+	currentMode = "default";
+	colorChange(e.target.value);
+};
+
+eraserBtn.onclick = () => (currentMode = "eraser");
+rainbowBtn.onclick = () => {
+	currentMode = "rainbow";
+	setCurrentColor();
+};
 
 gridSizeBtn.addEventListener("click", function () {
 	gridSize = prompt("Enter grid size", `24`);
@@ -16,7 +25,7 @@ gridSizeBtn.addEventListener("click", function () {
 	addGrid(gridSize);
 });
 
-function addGrid(gridSize) {
+function addGrid(gridSize = 16) {
 	grid.setAttribute(
 		"style",
 		`max-width: ${gridSize * 31}px; max-height: ${gridSize * 31}px;`
@@ -26,26 +35,45 @@ function addGrid(gridSize) {
 		divBox.setAttribute("class", "grid-element");
 		grid.appendChild(divBox);
 	}
-	colorChange(currentColor);
+	colorChange();
 }
 
-function colorChange(currentColor) {
+function colorChange(currentColor = "#333333") {
 	let nodeList = document.querySelectorAll(".grid-element");
 
 	gridElement = Array.from(nodeList);
 
 	gridElement.forEach((element) => {
 		element.addEventListener("mouseenter", function () {
-			this.style.backgroundColor = currentColor;
+			if (currentMode == "rainbow") {
+				this.style.backgroundColor = `${randomColor()}`;
+			} else if (currentMode == "default") {
+				this.style.backgroundColor = currentColor;
+			} else if (currentMode == "eraser") {
+				this.style.backgroundColor = "#FFFFFF";
+			}
 		});
 	});
 }
 
-function setCurrentColor(newColor) {
-	currentColor = newColor;
-	colorChange(currentColor);
+function randomInt(max = 1, min = 0) {
+	// scale random number from 0 to 1 to desired min and max
+	return parseInt(Math.random() * (max - min) + min);
 }
 
-// gridElement.addEventListener('mouseenter', () => {
-//     this.StylePropertyMap.cssText = 'background-color: black;'
-// })
+function twoPlaces(sNum = "") {
+	// make sure all strings have a length greater than 1
+	//   eg: "f" => "0f"
+	return sNum.length > 1 ? sNum : twoPlaces("0" + sNum);
+}
+
+function randomColor() {
+	// make each color's hex string
+	let r = twoPlaces(randomInt(255, 0).toString(16));
+	let g = twoPlaces(randomInt(255, 0).toString(16));
+	let b = twoPlaces(randomInt(255, 0).toString(16));
+	// return hex color string
+	return `#${r}${g}${b}`;
+}
+
+document.onload = addGrid();
